8f957bc64009f99d422e54e0828cb495aac5609b
Arm37e3 <82191653+Arm37e3@users.noreply.github.com>
2023-02-13 19:32:54 +0700

    Update test cal.py

--- test cal.py
+++ test cal.py
@@ -1,32 +1,71 @@
 from tkinter import *
+Calculator = Tk()
+Calculator.title("Calculator")
 
-window = Tk()
-window.geometry("530x330")
-window.maxsize(530,330)
-window.minsize(530,330)
-window.title("Calculate")
+content = ""
+txt_input = StringVar(value="")
 
+content2 = ""
+txt_input2 = StringVar(value="")
 
+content3 = ""
+txt_input3 = StringVar(value="")
+total = ""
+operators = ""
+def _plus():
+    global txt_input, txt_input2, txt_input3,operators,total
+   
+    operators="+"
+    total = display1.get()+operators+ display2.get()
+    print(total)
+    
+    ans = eval(total)
+    txt_input3.set(ans)
+    
+    
+def _minus():
+    global txt_input, txt_input2, txt_input3,operators,total
+   
+    operators="-"
+    total = display1.get()+operators+ display2.get()
+    print(total)
+    
+    ans = eval(total)
+    txt_input3.set(ans)    
+    
+def _multiply():
+    global txt_input, txt_input2, txt_input3,operators,total
+   
+    operators="*"
+    total = display1.get()+operators+ display2.get()
+    print(total)
+    
+    ans = eval(total)
+    txt_input3.set(ans)     
+    
+def _divide():
+    global txt_input, txt_input2, txt_input3,operators,total
+   
+    operators="/"
+    total = display1.get()+operators+ display2.get()
+    print(total)
+    
+    ans = eval(total)
+    txt_input3.set(ans) 
+    
+    
+display1 = Entry(font=('arial',30,'bold'),textvariable=txt_input, fg="white" , bg="pink" )
+display1.grid(columnspan=4)
+display2 = Entry(font=('arial',30,'bold'),textvariable=txt_input2, fg="white" , bg="red" )
+display2.grid(columnspan=4)
 
+display3 = Entry(font=('arial',30,'bold'),textvariable=txt_input3, fg="white" , bg="yellow" )
+display3.grid(columnspan=4)
 
+    
+btn_p1 = Button(fg="black",font=("arial",30,"bold ") ,text="+",padx=30,pady=15,command=_plus).grid(row=3,column=1)
+btn_p2 = Button(fg="black",font=("arial",30,"bold ") ,text="-",padx=30,pady=15,command=_minus).grid(row=3,column=2)
+btn_p3 = Button(fg="black",font=("arial",30,"bold ") ,text="x",padx=30,pady=15,command=_multiply).grid(row=3,column=3)
+btn_p4 = Button(fg="black",font=("arial",30,"bold ") ,text="/",padx=30,pady=15,command=_divide).grid(row=3,column=4)
 
-display = Entry(font=('arial',16))
-
-in1 = Text(window,width=10,height=1)
-in1.place(x=250,y=70)
-in2 = Text(window,width=10,height=1)
-in2.place(x=250,y=90)
-op = Text(window,width=10,height=1)
-op.place(x=250,y=120)
-
-btnmultiply = Button(window,width=1,height=1,font=('arial',7,'bold'),text='x',command=lambda:btn).place(x=290,y=160)
-btnplus = Button (window,width=1,height=1,font=('arial',7,'bold'),text='+',command=lambda:btn).place(x=250,y=160)
-btndivide = Button(window,width=1,height=1,font=('arial',7,'bold'), text="÷",command=lambda:btn).place(x=310,y=160)
-btnsubtract = Button(window,width=1,height=1,font=('arial',7,'bold'), text="-",command=lambda:btn).place(x=270,y=160)
-
-
-Button(window,text="คำนวณ",fg='green').place(x=230,y=200)
-Button(window,text="Clear",fg='red').place(x=290,y=200)
-
-window.mainloop()
-
+Calculator.mainloop()
